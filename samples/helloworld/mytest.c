a3d70cd7be71e257821a7c7d53b8921e75fe9b65
justin.zhao <zhaojunhua@hisilicon.com>
2018-05-03 10:59:03 +0800

    Add two threads
    Signed-off-by: justin.zhao <zhaojunhua@hisilicon.com>

--- samples/helloworld/mytest.c
+++ samples/helloworld/mytest.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <thread>
 #include <iostream>
+#include <unistd.h>
 
-void foo()
+void foo(int index)
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Hello, World! index=" << index << std::endl;
+	sleep(5);
 }
 
 int main(int argc, char *argv[])
@@ -19,11 +21,16 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+
+	std::thread t0(foo, 0);
+	std::thread t1(foo, 1);
+
     for(i=0; i<5; i++)
         printf("Justin.Zhao: test %d\r\n", i);
 
-	std::thread t(foo);
-	t.join();
+	t0.join();
+	t1.join();
+	std::cout << "End!" << std::endl;
 
 	return 0;
 }
